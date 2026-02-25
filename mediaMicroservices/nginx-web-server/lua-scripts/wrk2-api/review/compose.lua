5604f6daec2417f4c715b1ca454baa15b25fcc66
Jonas Konrath <konrathj@ethz.ch>
2026-02-25 16:53:56 +0100

    aaaa

--- mediaMicroservices/nginx-web-server/lua-scripts/wrk2-api/review/compose.lua
+++ mediaMicroservices/nginx-web-server/lua-scripts/wrk2-api/review/compose.lua
@@ -21,7 +21,7 @@ local function _UploadText(req_id, post, carrier)
   local GenericObjectPool = require "GenericObjectPool"
   local TextServiceClient = require 'media_service_TextService'
   local text_client = GenericObjectPool:connection(
-    TextServiceClient, "text-service" .. k8s_suffix, 9099) -- 24.2.2026 change from 9090 because that is a reserved port in knative
+    TextServiceClient, "text-service" .. k8s_suffix, 80) -- 24.2.2026 change from 9090 because that is a reserved port in knative
   text_client:UploadText(req_id, post.text, carrier)
   GenericObjectPool:returnConnection(text_client)
 end
