e38fd4aeb1d5daad6613bfcc0c40dbeeea2b281e
Jonas Konrath <konrathj@ethz.ch>
2026-04-29 15:51:55 +0200

    now with lua port 80 switching

--- socialNetwork/docker/openresty-thrift/lua-thrift/RpcClient.lua
+++ socialNetwork/docker/openresty-thrift/lua-thrift/RpcClient.lua
@@ -23,6 +23,16 @@ end
 
 --初始化RPC连接
 function RpcClient:init(ip, port, timeout, ssl)
+	local serverless = os.getenv("serverless")
+	-- below two "lists" of service names could be different in the future
+	-- could prepare multiple to be serverless and switch to http, but not
+	-- neccessarily deploying all at once on knative
+	local dest_is_http = (IsIpServiceName(ip, "user-service") or false)
+	local dest_is_knative = serverless and (IsIpServiceName(ip, "user-service") or false)
+
+	if dest_is_knative then
+		port = 80 -- make knative happy
+	end
 	if (ssl == true) then
 		socket = TSocketSSL:new {
 			host = ip,
@@ -38,7 +48,7 @@ function RpcClient:init(ip, port, timeout, ssl)
 	local transport = TFramedTransport:new {
 		trans = socket
 	}
-	if IsIpServiceName(ip, "user-service") then
+	if dest_is_http then
 		transport = THttpTransport:new {
 			trans = socket,
 			isServer = false,
