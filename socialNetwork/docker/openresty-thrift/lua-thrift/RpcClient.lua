6dca545542c2fe3c309cb0a17715ef251ca37bad
Jonas Konrath <konrathj@ethz.ch>
2026-04-29 16:35:16 +0200

    hehe

--- socialNetwork/docker/openresty-thrift/lua-thrift/RpcClient.lua
+++ socialNetwork/docker/openresty-thrift/lua-thrift/RpcClient.lua
@@ -23,7 +23,7 @@ end
 
 --初始化RPC连接
 function RpcClient:init(ip, port, timeout, ssl)
-	local serverless = os.getenv("serverless")
+	local serverless = os.getenv("serverless") == "true"
 	-- below two "lists" of service names could be different in the future
 	-- could prepare multiple to be serverless and switch to http, but not
 	-- neccessarily deploying all at once on knative
