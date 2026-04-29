6a0aa7b13408466533db5ebf65088dc13fcd93e2
Jonas Konrath <konrathj@ethz.ch>
2026-04-30 00:04:43 +0200

    omg

--- socialNetwork/docker/openresty-thrift/lua-thrift/GenericObjectPool.lua
+++ socialNetwork/docker/openresty-thrift/lua-thrift/GenericObjectPool.lua
@@ -8,30 +8,32 @@ local ngx = ngx
 local GenericObjectPool = Object:new({
     __type = 'GenericObjectPool',
     maxTotal = 100,
-    maxIdleTime = 10000,
+    maxIdleTime = 60000,
     timeout = 10000
-    })
+})
 function GenericObjectPool:init(conf)
 end
+
 --
 --从连接池获取rpc客户端
 --ngx nginx容器变量
 --
-function GenericObjectPool:connection(thriftClient,ip,port)
+function GenericObjectPool:connection(thriftClient, ip, port)
     local ssl = ngx.shared.config:get("ssl")
-    local client = RpcClientFactory:createClient(thriftClient,ip,port,self.timeout,ssl)
+    local client = RpcClientFactory:createClient(thriftClient, ip, port, self.timeout, ssl)
     return client
 end
+
 --
 --回收连接资源到连接池
 --client rpc客户端对象
 --
 function GenericObjectPool:returnConnection(client)
-    if(client ~= nil)then
-        if (client.iprot.trans.trans:isOpen())then
+    if (client ~= nil) then
+        if (client.iprot.trans.trans:isOpen()) then
             client.iprot.trans.trans:setKeepAlive(self.maxIdleTime, self.maxTotal)
         else
-            ngx.log(ngx.ERR,"return rpc client fail, socket close.")
+            ngx.log(ngx.ERR, "return rpc client fail, socket close.")
         end
     end
 end
@@ -54,7 +56,9 @@ end
 function GenericObjectPool:clear()
 
 end
+
 function GenericObjectPool:remove()
 
 end
+
 return GenericObjectPool
